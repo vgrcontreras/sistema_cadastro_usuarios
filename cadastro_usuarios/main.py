a9cff6c54504d3e8f89508842b32777aceac8ed0
Victor Contreras <contreras3991@gmail.com>
2024-09-10 12:36:07 -0300

    :construction: feat: create HTTP POST method in users endpoint for creating users

--- cadastro_usuarios/main.py
+++ cadastro_usuarios/main.py
@@ -1,7 +1,21 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from cadastro_usuarios.schemas import UserSchema, UserSchemaPublic
 
 app = FastAPI()
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Hello World'}
+
+
+@app.post(
+    '/users',
+    status_code=HTTPStatus.CREATED,
+    response_model=UserSchemaPublic,
+)
+def create_user(user: UserSchema):
+    return user
